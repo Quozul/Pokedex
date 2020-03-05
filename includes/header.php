252cdcb8cb65d32fc58d8b77f61f688e53f9cf3a
hemarydem <43240309+hemarydem@users.noreply.github.com>
2020-03-05 08:16:44 +0100

    Add files via upload
    Remise à zéro du repository

--- includes/header.php
+++ includes/header.php
@@ -2,23 +2,15 @@
     <?php
 
     include('config.php');
-
-    if (isset($_SESSION['userid'])) {
-        $sth = $pdo->prepare('SELECT pseudo FROM user WHERE id = ? ');
-
-        $sth->execute([$_SESSION['userid']]);
-
-        $rec = $sth->fetch();
-    }
     ?>
-    <a>Pokedex</a>
+
     <nav>
         <ul>
-            <li>Acceuil</li>
-            <li>Collection</li>
-            <li>Connexion/déconnexion</li>
-            <li>Ajouter un Pokemon</li>
-            <li>Mon compte</li>
+            <li><a href="">Acceuil</a></li>
+            <li><a href="">Collection</a></li>
+            <li><a href="">Connexion/déconnexion</a></li>
+            <li><a href="">Ajouter un Pokemon</a></li>
+            <li><a href="">Mon compte</a></li>
         </ul>
     </nav>
 </header>
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require('includes/config.php');
+?>
+<!doctype html>
+<html>
+
+<?php include('includes/head.php'); ?>
+
+<body>
+
+    <?php
+    include('includes/header.php');
+    ?>
+
+    <?php
+    include('includes/footer.php');
+    ?>
+</body>
+
+</html>
